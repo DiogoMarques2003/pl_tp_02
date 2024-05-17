4fb06d4d69de347b00429a41edd6caca6249dedd
Diogo Marques <diogomarques2003@protonmail.com>
2024-05-17 23:19:18 +0100

    Criação do lexer

--- arith_lexer.py
+++ arith_lexer.py
@@ -2,4 +2,120 @@ import ply.lex as plex
 
 
 class ArithLexer:
-    print("Teste")
+    tokens = (
+        "NUM",  # numero inteiro
+        "STRING",  # string delimitada por aspas duplas
+        "FIM",  # atribuicao para o "FIM" das funções
+        "FUNCAO",  # identificador de uma função
+        "ESCREVER",  # identificador
+        "VAR",  # atribuicao de uma variavel
+        "COMENTARIO",  # identificador de um comentario
+        "SE",  # identificador de uma confição if
+        "SENAO",  # identificador de uma condição else
+        "NEG",  # Negação da condição do if
+        "ENTRADA",  # Identificador para input de dados
+    )
+
+    literals = [
+        '+',  # sinal de soma
+        '-',  # sinal de subtração
+        '*',  # sinal de multiplicação
+        '/',  # sinal de divisão
+        '=',  # sinal de igual
+        '(',  # parêntese esquerdo
+        ')',  # parêntese direito
+        '[',  # parêntese reto esquerdo
+        ']',  # parêntese reto direito
+        '>',  # sinal de maior que
+        '<',  # sinal de menor que
+        ',',  # vírgula
+        ';',  # ponto-e-vírgula
+    ]
+
+    # Ignorar espacos
+    t_ignore = " "
+
+    # Inicializa o lexer como None
+    def __init__(self):
+        self.lexer = None
+
+    # Reconhecer numeros inteiros e decimais
+    def t_NUM(self, t):
+        r"[0-9]+(\.[0-9]+)?"
+        t.value = int(t.value)
+        return t
+
+    # Reconhecer a Palavra ESC ou ESCREVER
+    def t_ESCREVER(self, t):
+        r"ESC(REVER)?"
+        return t
+
+    # Reconhecer uma variavel
+    def t_VAR(self, t):
+        # Deve iniciar por letra minuscula ou _
+        # Depois pode ter qualquer letra minuscula/maiuscula e números
+        # Pode terminar com ? ou !
+        r"[a-z_][a-zA-Z0-9_]*[?!]?"
+        return t
+
+    # Reconhecedor de FIM
+    def t_FIM(self, t):
+        r"FIM"
+        return t
+
+    # Reconhecedor de FUNCAO
+    def t_FUNCAO(self, t):
+        r"FUNCAO"
+        return t
+
+    # Reconhecedor de uma string
+    def t_STRING(self, t):
+        # Usamos o ? para evitar o * selecionar mais coisas do que o que deve
+        r'".*?"'
+        t.value = t.value[1:-1]  # Remove aspas
+        return t
+
+    #Reconhecedor de COMENTARIO
+    def t_COMENTARIO(self, t):
+        # Reconhece comentários de uma linha iniciados por '--'
+        # Reconhece comentários multilinha delimitados por '{--' e '--}'
+        r"(--[^\n]*|{--.*?--})"
+        return t
+
+    #Reconhecedor de SE
+    def t_SE(self, t):
+        r"SE"
+        return t
+
+    #Reconhecedor de SENAO
+    def t_SENAO(self, t):
+        r"SENAO"
+        return t
+
+    #Reconhecedor de NEG (negação da condição)
+    def t_NEG(self, t):
+        r"NEG"
+        return t
+
+    #Reconhecedor de ENTRADA
+    def t_ENTRADA(self, t):
+        r"(ENT)RADA"
+        return t
+
+    # cria o lexer
+    def build(self, **kwargs):
+        self.lexer = plex.lex(module=self, **kwargs)
+
+    # define a entrada do lexer
+    def input(self, string):
+        self.lexer.input(string)
+
+    # Obter proximo token do lexer
+    def token(self):
+        token = self.lexer.token()  # percorrer todos os tokens
+        return token if token is None else token.type
+
+    # Ocorre quando ocorre erro lexico
+    def t_error(self, t):
+        print(f"Unexpected token: [{t.value[:10]}]")
+        exit(1)
