329d3a824a44f125dc9900756c9d195a36105297
Diogo Marques <diogomarques2003@protonmail.com>
2024-05-18 09:44:31 +0100

    Tester para o lexer

---
+++ arith_lexer_test.py
@@ -0,0 +1,45 @@
+# arith_lexer_test.py
+from arith_lexer import ArithLexer
+
+al = ArithLexer()
+al.build()
+al.input("(3-6)+9")  # "2++3") #"(3+5)*7")
+
+while True:
+    tk = al.token()
+    if not tk:
+        break
+    print(tk, end="")
+
+print("\n-------\n")
+al.input("num+2")  # "2++3") #"(3+5)*7")
+
+while True:
+    tk = al.token()
+    if not tk:
+        break
+    print(tk, end="")
+
+print("\n-------\n")
+al.input(""" x = 10  ; 
+   y = 10 + 20 * 30;
+   z = x * 100 ; 
+   b =  a + 1 ; """)
+
+while True:
+    tk = al.token()
+    if not tk:
+        break
+    print(tk, end="")
+
+print("\n-------\n")
+al.input("""_cart? = 10, _cart! = 20;
+            ESC(_cart+_cart)""")
+
+while True:
+    tk = al.token()
+    if not tk:
+        break
+    print(tk, end="")
+
+
